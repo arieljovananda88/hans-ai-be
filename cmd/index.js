e84c965f66a90510ff7c917585ec7604309188e5
HoseaNA <13521057@std.stei.itb.ac.id>
2024-11-24 21:12:47 +0700

    merge dev

--- cmd/index.js
+++ cmd/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
 const healthCheckRouter = require('../module/health-check/routes')
+const usersRouter = require('../module/users/routes')
+const pantryItemsRouter = require('../module/pantry-items/routes')
 
 app.use('/', healthCheckRouter)
+app.use('/users', usersRouter)
+app.use('/pantry-items', pantryItemsRouter)
 
 app.listen(3000, () => {
   console.log('Server is listening on port 3000')
